4addcc4dd73ca0c752c29f2ed70c191d52135548
maisonleder-dev <maisonleder@gmail.com>
2026-05-21 16:15:06 +0200

    ajout route index

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,9 @@ import requests
 
 app = Flask(__name__)
 CORS(app)
+@app.route("/")
+def index():
+    return jsonify({"status": "InstaDown backend en ligne ✅"})
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
